36eeeb7629d1a584f58338a5b30bc7fb68a57615
Ahmed Okasha <75159786+A-Oka-jr@users.noreply.github.com>
2024-08-29 07:43:33 +0400

    getting compny profile data

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,7 +1,45 @@
 import { useSelector } from "react-redux";
+import { useEffect, useState } from "react";
+import axios from "axios";
+
 const Profile = () => {
   const { currentUser } = useSelector((state) => state.user);
-  console.log(currentUser.user);
+  const [company, setCompany] = useState({});
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    if (!currentUser) {
+      window.location.href = "/login";
+    }
+    const getCompanyProfile = async () => {
+      setError(false);
+      setLoading(true);
+
+      try {
+        const response = await axios.get(
+          `/api/v1/company/get_by_id/${currentUser.user.company_id}`
+        );
+
+        const data = response;
+
+        if (data.success === false) {
+          console.log(data.message);
+          setLoading(false);
+          setError(true);
+          return;
+        }
+        setLoading(false);
+        setCompany(data.data);
+      } catch (error) {
+        console.error(error);
+        setError(true);
+      }
+    };
+    if (currentUser.user.role === "company_user") {
+      getCompanyProfile();
+    }
+  }, [currentUser.user.company_id]);
 
   return (
     <div className="container mx-auto p-8">
@@ -14,7 +52,7 @@ const Profile = () => {
         <div>
           <h1 className="text-3xl font-bold">{currentUser.name}</h1>
           <span className="text-blue-500">
-            {currentUser.role === "job_seeker" ? "Job Seeker" : "Company"}
+            {currentUser.role === "job_seeker" ? "Job Seeker" : company.name}
           </span>
         </div>
       </div>
@@ -24,13 +62,13 @@ const Profile = () => {
         <div className="bg-white p-6 shadow rounded-lg">
           <h2 className="text-2xl font-bold mb-4">General Information</h2>
           <p>
-            <strong>Email:</strong> {currentUser.email}
+            <strong>Email:</strong> {company.email}
           </p>
           <p>
-            <strong>Phone:</strong> {currentUser.phone}
+            <strong>Phone:</strong> {company.phone}
           </p>
           <p>
-            <strong>Address:</strong> {currentUser.address}
+            <strong>Address:</strong> {company.address}
           </p>
         </div>
 
@@ -57,17 +95,20 @@ const Profile = () => {
           <div className="bg-white p-6 shadow rounded-lg">
             <h2 className="text-2xl font-bold mb-4">Company Overview</h2>
             <p>
-              <strong>Industry:</strong> {currentUser.industry}
+              <strong>Industry:</strong> {company.industry}
             </p>
             <p>
-              <strong>Company Size:</strong> {currentUser.companySize}
+              <strong>Company Size:</strong> {company.companySize}
             </p>
             {/* <p>
               <strong>Open Positions:</strong>{" "}
-              {currentUser.openPositions.length}
+              {company.openPositions.length}
             </p> */}
           </div>
         )}
+        <button className="bg-blue-500 text-white px-4 py-2 rounded">
+          Edit
+        </button>
       </div>
 
       {/* Additional sections like Settings can be added here */}
